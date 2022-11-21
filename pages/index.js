bfc25930546a005078b087e43c3a9c3e0d3a4966
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2022-11-21 14:30:02 +0000

    image fixes in resonsive

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,7 @@ export default function Home() {
               <AiFillLinkedin />
               <AiFillYoutube />
             </div>
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-[50vw] h-[28vh] relative overflow-hidden mt-20 md:h-96 md:w-96">
               <Image src={deved} layout="fill" objectFit="cover" />
             </div>
           </div>
