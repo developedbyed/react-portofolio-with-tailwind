2203be022286cacb6efa298bd80fd03195e871d6
Kenneth Lo <72805908+HkgCory@users.noreply.github.com>
2023-03-08 10:21:30 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
